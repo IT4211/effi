8c2fc9d569be1f6b72e93aa665f55fe516eb862b
IT4211 <rhdqor100@live.co.kr>
2017-01-06 02:43:42 +0900

    Update _conf.py
    중위 표기 조건식을 후위 표기식으로 변환 후, 후위 표기식 계산하는 함수 구현 완료

--- _conf.py
+++ _conf.py
@@ -3,84 +3,67 @@ import ConfigParser # cfg file parsing
 
 def extractconf():
 
-    config = ConfigParser.ConfigParser()
-    config.read('option.cfg')
-    if config.has_section('extract_option'):
-        options = config.items('extract_option')
+	config = ConfigParser.ConfigParser()
+	config.read('option.cfg')
+	if config.has_section('extract_option'):
+		options = config.items('extract_option')
 
-    return options
-
-str_to_token = {'True':True,
-                'False':False,
-                'and':lambda left, right: left and right,
-                'or':lambda left, right: left or right,
-                '(':'(',
-                ')':')'}
-
-empty_res = True
+	return options
 
 
-def create_token_lst(s, str_to_token=str_to_token):
-    """create token list:
-    'True or False' -> [True, lambda..., False]"""
-    s = s.replace('(', ' ( ')
-    s = s.replace(')', ' ) ')
+def infix_to_postfix(infix_exp):
+	stack = []
+	postfix = []
+	relational = ['>', '<', '=', '>=', '<=', '!=']
+	opnd = ['mtime', 'atime', 'ctime', 'etime', 'ext', 'size', 'path']
+	logicalop = ['and', 'or']
 
-    return [str_to_token[it] for it in s.split()]
+	infix_exp = infix_exp.replace('(', '( ')
+	infix_exp = infix_exp.replace(')', ' )')
 
+	split_exp = infix_exp.split(' ')
+	for i in split_exp:
+		if(len(stack) == 0): is_empty = True
+		else: is_empty = False
 
-def find(lst, what, start=0):
-    return [i for i,it in enumerate(lst) if it == what and i >= start]
+		if i in relational:
+			stack.append(i)
 
+		elif i in logicalop:
+			peek = stack[-1:]
+			if (not is_empty) and (peek in relational):
+				postfix.append(stack.pop())
+				stack.append(i)
 
-def parens(token_lst):
-    """returns:
-        (bool)parens_exist, left_paren_pos, right_paren_pos
-    """
-    left_lst = find(token_lst, '(')
+		elif i == '(':
+			stack.append(i)
 
-    if not left_lst:
-        return False, -1, -1
-
-    left = left_lst[-1]
-
-    #can not occur earlier, hence there are args and op.
-    right = find(token_lst, ')', left + 4)[0]
-
-    return True, left, right
-
-
-def bool_eval(token_lst):
-    """token_lst has length 3 and format: [left_arg, operator, right_arg]
-    operator(left_arg, right_arg) is returned"""
-    return token_lst[1](token_lst[0], token_lst[2])
+		elif i == ')':
+			top_op = stack.pop()
+			while top_op != '(':
+				postfix.append(top_op)
+				top_op = stack.pop()
 
 
-def formatted_bool_eval(token_lst, empty_res=empty_res):
-    """eval a formatted (i.e. of the form 'ToFa(ToF)') string"""
-    if not token_lst:
-        return empty_res
+		else:
+			postfix.append(i)
 
-    if len(token_lst) == 1:
-        return token_lst[0]
+	while len(stack):
+		postfix.append(stack.pop())
 
-    has_parens, l_paren, r_paren = parens(token_lst)
+	return " ".join(postfix)
 
-    if not has_parens:
-        return bool_eval(token_lst)
+def search_condition(postfix_exp):
+	exp = postfix_exp.split(" ")
+	stack = []
+	op = ['>', '<', '=', '>=', '<=', '!=', 'and', 'or']
+	for i in exp:
+		if i not in op:
+			stack.append(i)
+		else:
+			op2 = stack.pop()
+			op1 = stack.pop()
+			exp = op1 + " " + i + " " + op2
+			stack.append(eval(exp))
 
-    token_lst[l_paren:r_paren + 1] = [bool_eval(token_lst[l_paren+1:r_paren])]
-
-    return formatted_bool_eval(token_lst, bool_eval)
-
-
-def nested_bool_eval(s):
-    """The actual 'eval' routine,
-    if 's' is empty, 'True' is returned,
-    otherwise 's' is evaluated according to parentheses nesting.
-    The format assumed:
-        [1] 'LEFT OPERATOR RIGHT',
-        where LEFT and RIGHT are either:
-                True or False or '(' [1] ')' (subexpression in parentheses)
-    """
-    return formatted_bool_eval(create_token_lst(s))
+	return stack.pop()
